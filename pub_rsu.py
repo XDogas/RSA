9c46fda49442e7ed84eedb36430d0e15d1e98d2b
João Diogo Videira Oliveira <jdvoliveira@ua.pt>
2022-05-19 00:26:42 +0100

    update python script to publish RSU CAMs

--- pub_rsu.py
+++ pub_rsu.py
@@ -3,6 +3,7 @@ import paho.mqtt.client as mqtt
 import time
 import sys
 import logging
+import jsonFile
 
 # client logging function
 def on_log(client, userdata, level, buf):
@@ -61,9 +62,12 @@ while not client.connected_flag:    # wait for connection
 print("subscribing to " + topic)
 client.subscribe(topic)
 
+dataStr = jsonFile.toStr("examples/in_cam.json")
+
 while True:
     print("publishing")
-    res = client.publish(topic, '{"accEngaged": true,"acceleration": 0,"altitude": 800001,"altitudeConf": 15,"brakePedal": true,"collisionWarning": true,"cruiseControl": true,"curvature": 1023,"driveDirection": "FORWARD","emergencyBrake": true,"gasPedal": false,"heading": 3601,"headingConf": 127,"latitude": 400000000,"length": 100,"longitude": -80000000,"semiMajorConf": 4095,"semiMajorOrient": 3601,"semiMinorConf": 4095,"specialVehicle": {"publicTransportContainer": {"embarkationStatus": false}},"speed": 16383,"speedConf": 127,"speedLimiter": true,"stationID": 1,"stationType": 15,"width": 30,"yawRate": 0}')
+    # res = client.publish(topic, '{"accEngaged": true,"acceleration": 0,"altitude": 800001,"altitudeConf": 15,"brakePedal": true,"collisionWarning": true,"cruiseControl": true,"curvature": 1023,"driveDirection": "FORWARD","emergencyBrake": true,"gasPedal": false,"heading": 3601,"headingConf": 127,"latitude": 400000000,"length": 100,"longitude": -80000000,"semiMajorConf": 4095,"semiMajorOrient": 3601,"semiMinorConf": 4095,"specialVehicle": {"publicTransportContainer": {"embarkationStatus": false}},"speed": 16383,"speedConf": 127,"speedLimiter": true,"stationID": 1,"stationType": 15,"width": 30,"yawRate": 0}')
+    res = client.publish(topic, dataStr)
     if not res[0]==0:
         break
     time.sleep(1)
