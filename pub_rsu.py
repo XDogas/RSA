be0da0684f9dc99d945a44ff704ed77fc1df2fc8
João Diogo Videira Oliveira <jdvoliveira@ua.pt>
2022-05-18 20:26:35 +0100

    update python script to publish RSU CAMs every second

--- pub_rsu.py
+++ pub_rsu.py
@@ -66,7 +66,7 @@ while True:
     res = client.publish(topic, '{"accEngaged": true,"acceleration": 0,"altitude": 800001,"altitudeConf": 15,"brakePedal": true,"collisionWarning": true,"cruiseControl": true,"curvature": 1023,"driveDirection": "FORWARD","emergencyBrake": true,"gasPedal": false,"heading": 3601,"headingConf": 127,"latitude": 400000000,"length": 100,"longitude": -80000000,"semiMajorConf": 4095,"semiMajorOrient": 3601,"semiMinorConf": 4095,"specialVehicle": {"publicTransportContainer": {"embarkationStatus": false}},"speed": 16383,"speedConf": 127,"speedLimiter": true,"stationID": 1,"stationType": 15,"width": 30,"yawRate": 0}')
     if not res[0]==0:
         break
-    time.sleep(4)
+    time.sleep(1)
     
 client.loop_stop()                  # stop loop
 client.disconnect()                 # disconnect
