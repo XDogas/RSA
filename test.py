6374d4e676f7a5b50eb11686045359d8acd482a3
João Diogo Videira Oliveira <jdvoliveira@ua.pt>
2022-05-18 15:46:13 +0100

    fix test.py to publish RSU CAM

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
-broker_address="192.168.98.10" # ADICIONEI
+broker_address="192.168.98.10" # ADICIONEI, Ẽ O ADDR DA RSU
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -21,10 +21,11 @@ client.on_message = on_message
 # client.connect("mqtt.eclipseprojects.io", 1883, 60)
 client.connect(broker_address, 1883, 60) # ADICIONEI EM VEZ DA LINHA ANTERIOR (VER SE SE TIRA OS 2 ULTIMOS ARGS)
 
-client.publish('{"accEngaged": true,"acceleration": 0,"altitude": 800001,"altitudeConf": 15,"brakePedal": true,"collisionWarning": true,"cruiseControl": true,"curvature": 1023,"driveDirection": "FORWARD","emergencyBrake": true,"gasPedal": false,"heading": 3601,"headingConf": 127,"latitude": 400000000,"length": 100,"longitude": -80000000,"semiMajorConf": 4095,"semiMajorOrient": 3601,"semiMinorConf": 4095,"specialVehicle": {"publicTransportContainer": {"embarkationStatus": false}},"speed": 16383,"speedConf": 127,"speedLimiter": true,"stationID": 1,"stationType": 15,"width": 30,"yawRate": 0}') # ADICIONEI MAS ESTÁ MAL
+client.publish("vanetza/in/cam",'{"accEngaged": true,"acceleration": 0,"altitude": 800001,"altitudeConf": 15,"brakePedal": true,"collisionWarning": true,"cruiseControl": true,"curvature": 1023,"driveDirection": "FORWARD","emergencyBrake": true,"gasPedal": false,"heading": 3601,"headingConf": 127,"latitude": 400000000,"length": 100,"longitude": -80000000,"semiMajorConf": 4095,"semiMajorOrient": 3601,"semiMinorConf": 4095,"specialVehicle": {"publicTransportContainer": {"embarkationStatus": false}},"speed": 16383,"speedConf": 127,"speedLimiter": true,"stationID": 1,"stationType": 15,"width": 30,"yawRate": 0}') # ADICIONEI MAS ESTÁ MAL
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
 # Other loop*() functions are available that give a threaded interface and a
 # manual interface.
-client.loop_forever()
+# client.loop_forever()
+client.disconnect()
